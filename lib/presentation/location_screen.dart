bbee2e76c23b051cb701b73d19e760d474388a68
SaurabhS120 <saurabh.sonar120@gmail.com>
2022-10-31 11:55:27 +0530

    Choose city back press

--- lib/presentation/location_screen.dart
+++ lib/presentation/location_screen.dart
@@ -26,92 +26,102 @@ class LocationScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Stack(
-        children: [
-          Container(
-            width: double.maxFinite,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Obx(() => Text('Selected location : $city')),
-                SizedBox(
-                  height: 24,
-                ),
-                ElevatedButton(
-                    onPressed: () {
-                      geoLocationController.getLocation();
-                    },
-                    child: Text('Get city from location')),
-                SizedBox(
-                  height: 12,
-                ),
-                Text('OR'),
-                SizedBox(
-                  height: 12,
-                ),
-                ElevatedButton(
-                    onPressed: () {
-                      _choosing_loc.value = true;
-                    },
-                    child: Text('Choose City'))
-              ],
-            ),
-          ),
-          Container(
-            padding: EdgeInsets.all(12),
-            alignment: Alignment.bottomCenter,
-            child: SizedBox(
+      body: WillPopScope(
+        onWillPop: () {
+          if (_choosing_loc.isTrue) {
+            _choosing_loc.value = false;
+            return Future.value(false);
+          } else {
+            return Future.value(true);
+          }
+        },
+        child: Stack(
+          children: [
+            Container(
               width: double.maxFinite,
-              child: ElevatedButton(
-                onPressed: () {
-                  if (location.value == null) {
-                    Get.snackbar(
-                        "Location error", "Please select location to proceed");
-                  } else {
-                    weatherController.locationEntity = location.value!;
-                    Get.toNamed('/home');
-                  }
-                },
-                child: Text('Proceed with selected location '),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Obx(() => Text('Selected location : $city')),
+                  SizedBox(
+                    height: 24,
+                  ),
+                  ElevatedButton(
+                      onPressed: () {
+                        geoLocationController.getLocation();
+                      },
+                      child: Text('Get city from location')),
+                  SizedBox(
+                    height: 12,
+                  ),
+                  Text('OR'),
+                  SizedBox(
+                    height: 12,
+                  ),
+                  ElevatedButton(
+                      onPressed: () {
+                        _choosing_loc.value = true;
+                      },
+                      child: Text('Choose City'))
+                ],
               ),
             ),
-          ),
-          Obx(
-                () {
-              return Visibility(
-                  visible: fetching_location.isTrue,
-                  child: SizedBox(
-                    width: double.maxFinite,
-                    child: Container(
-                      decoration: BoxDecoration(
-                          color: Color.fromRGBO(255, 255, 255, 0.9)),
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          CircularProgressIndicator(),
-                          Text('Getting current location'),
-                        ],
+            Container(
+              padding: EdgeInsets.all(12),
+              alignment: Alignment.bottomCenter,
+              child: SizedBox(
+                width: double.maxFinite,
+                child: ElevatedButton(
+                  onPressed: () {
+                    if (location.value == null) {
+                      Get.snackbar("Location error",
+                          "Please select location to proceed");
+                    } else {
+                      weatherController.locationEntity = location.value!;
+                      Get.toNamed('/home');
+                    }
+                  },
+                  child: Text('Proceed with selected location '),
+                ),
+              ),
+            ),
+            Obx(
+              () {
+                return Visibility(
+                    visible: fetching_location.isTrue,
+                    child: SizedBox(
+                      width: double.maxFinite,
+                      child: Container(
+                        decoration: BoxDecoration(
+                            color: Color.fromRGBO(255, 255, 255, 0.9)),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            CircularProgressIndicator(),
+                            Text('Getting current location'),
+                          ],
+                        ),
                       ),
+                    ));
+              },
+            ),
+            Obx(
+              () => Visibility(
+                  visible: _choosing_loc.isTrue,
+                  child: Container(
+                    color: Colors.white,
+                    child: SelectState(
+                      onCityChanged: (value) {
+                        this.city.value = value;
+                        _choosing_loc.value = false;
+                      },
+                      onCountryChanged: (value) {},
+                      onStateChanged: (value) {},
                     ),
-                  ));
-            },
-          ),
-          Obx(
-            () => Visibility(
-                visible: _choosing_loc.isTrue,
-                child: Container(
-                  color: Colors.white,
-                  child: SelectState(
-                    onCityChanged: (value) {
-                      this.city.value = value;
-                      _choosing_loc.value = false;
-                    },
-                    onCountryChanged: (value) {},
-                    onStateChanged: (value) {},
-                  ),
-                )),
-          ),
-        ],
+                  )),
+            ),
+          ],
+        ),
       ),
     );
   }
