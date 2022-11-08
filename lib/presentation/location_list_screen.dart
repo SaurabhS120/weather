435fec0dc618c0b4af596d8c807347261a34062b
SaurabhS120 <saurabh.sonar120@gmail.com>
2022-11-08 13:28:02 +0530

    city list ui update

--- lib/presentation/location_list_screen.dart
+++ lib/presentation/location_list_screen.dart
@@ -18,68 +18,86 @@ class LocationListScreen extends StatelessWidget {
       appBar: AppBar(
         title: Text('Location list'),
       ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: [
-          Obx(
-            () => GridView.builder(
-              shrinkWrap: true,
-              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                crossAxisCount: 2,
-                mainAxisSpacing: 8,
-                crossAxisSpacing: 8,
-              ),
-              itemCount: locationListController.getCityList().length,
-              itemBuilder: (context, index) {
-                return Container(
-                  alignment: Alignment.topCenter,
-                  padding: EdgeInsets.all(4),
-                  decoration: BoxDecoration(
-                      color: Colors.black12,
-                      borderRadius: BorderRadius.all(Radius.circular(8))),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Text(
-                          locationListController.getCityList()[index].cityName),
-                      IconButton(
-                        icon: Icon(Icons.close),
-                        onPressed: () => locationListController.remove(index),
-                      ),
-                    ],
+      body: Container(
+        decoration: BoxDecoration(
+            gradient: LinearGradient(
+                colors: [Colors.black38, Colors.black54],
+                begin: Alignment(0, -1),
+                end: Alignment(0, 1))),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Obx(
+              () => Expanded(
+                child: GridView.builder(
+                  shrinkWrap: true,
+                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 2,
+                    mainAxisSpacing: 8,
+                    crossAxisSpacing: 8,
                   ),
-                );
-              },
+                  itemCount: locationListController.getCityList().length,
+                  itemBuilder: (context, index) {
+                    return Container(
+                      alignment: Alignment.topCenter,
+                      padding: EdgeInsets.fromLTRB(8, 0, 0, 0),
+                      decoration: BoxDecoration(
+                          color: Color.fromRGBO(0, 0, 0, 0.25098039215686274),
+                          borderRadius: BorderRadius.all(Radius.circular(8))),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(
+                            locationListController
+                                .getCityList()[index]
+                                .cityName,
+                            style: TextStyle(color: Colors.white),
+                          ),
+                          IconButton(
+                            icon: Icon(
+                              Icons.close,
+                              color: Colors.white,
+                            ),
+                            onPressed: () =>
+                                locationListController.remove(index),
+                          ),
+                        ],
+                      ),
+                    );
+                  },
                 ),
               ),
-              Column(
-                children: [
-                  ElevatedButton(
-                    onPressed: () {
-                      Get.toNamed('/location');
-                    },
-                    child: Text('Add city'),
-                  ),
-                  ElevatedButton(
-                    onPressed: () {
-                      if (locationListController.getCityList().isEmpty) {
-                        Get.snackbar("No locations",
-                            'Please add atleast one location to proceed');
-                      } else {
-                        locationController
-                        .setLocation(locationListController.getCityList()[0]);
-                    weatherController
-                        .set(locationListController.getCityList()[0]);
-                    Get.toNamed('/home');
-                  }
-                    },
-                    child: Text('Proceed'),
-                  ),
-                ],
-              )
-            ],
-          ),
-        ));
+            ),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                ElevatedButton(
+                  onPressed: () {
+                    Get.toNamed('/location');
+                  },
+                  child: Text('Add city'),
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    if (locationListController.getCityList().isEmpty) {
+                      Get.snackbar("No locations",
+                          'Please add atleast one location to proceed');
+                    } else {
+                      locationController
+                          .setLocation(locationListController.getCityList()[0]);
+                      weatherController
+                          .set(locationListController.getCityList()[0]);
+                      Get.toNamed('/home');
+                    }
+                  },
+                  child: Text('Proceed'),
+                ),
+              ],
+            )
+          ],
+        ),
+      ),
+    ));
   }
 }
